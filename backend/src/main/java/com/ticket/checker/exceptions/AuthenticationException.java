aacf20dd5777031360fc608c2483ec0747e77c58
alepopa <44004105+alepopa@users.noreply.github.com>
2019-05-08 23:41:28 +0300

    Add files via upload

--- backend/src/main/java/com/ticket/checker/exceptions/AuthenticationException.java
+++ backend/src/main/java/com/ticket/checker/exceptions/AuthenticationException.java
@@ -1,17 +1,10 @@
-package com.ticket.checker.exceptions;
+package com.ticket.checker.ticketchecker.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-/**
- * @author Daniel Incicau, daniel.incicau@busymachines.com
- * @since 01/04/2019
- */
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
+@SuppressWarnings("serial")
 public class AuthenticationException extends RuntimeException {
 
-    public AuthenticationException(String message) {
-        super(message);
-    }
+	public AuthenticationException(String message) {
+		super(message);
+	}
 
 }
---
+++ backend/src/main/java/com/ticket/checker/exceptions/TicketExistsException.java
@@ -0,0 +1,13 @@
+package com.ticket.checker.ticketchecker.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@SuppressWarnings("serial")
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class TicketExistsException extends RuntimeException {
+
+	public TicketExistsException(String message) {
+		super(message);
+	}
+}
